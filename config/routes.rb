587e77827e76eb997ae78c63be7be4f6d847c8b4
Theresa Ma <theresa.ma@mail.utoronto.ca>
2015-02-18 00:55:48 -0500

    popup signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root to: 'pages#home'
-  get 'signup'  => 'users#new'
+  
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :sign_up =>'signup'}
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
