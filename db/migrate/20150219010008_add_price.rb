b3182486ebf9e63b0f270617ce17c42a86d2fd29
Theresa Ma <theresa.ma@mail.utoronto.ca>
2015-03-05 02:32:49 -0500

    schema

--- db/migrate/20150219010008_add_price.rb
+++ db/migrate/20150219010008_add_price.rb
@@ -1,6 +1,6 @@
 class AddPrice < ActiveRecord::Migration
   def change
-    add_column :spaces, :description, :string
+    add_column :spaces, :description, :text
     add_column :spaces, :price, :decimal, :precision => 8, :scale => 2
   end
 end
