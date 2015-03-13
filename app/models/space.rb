7ece991f36454ee14afc7150d318fe19e98f62b0
Theresa Ma <theresa.ma@mail.utoronto.ca>
2015-03-12 21:55:30 -0400

    ?

--- app/models/space.rb
+++ app/models/space.rb
@@ -7,6 +7,7 @@ class Space < ActiveRecord::Base
 	default_scope -> { order(created_at: :desc) }
 	validates :user_id, presence: true
 	validates :name, presence: true, length: { maximum: 100 }
+	validates :description, presence: true, length: { maximum: 255 }
 	validates :spacetype, presence: true
 	validates :streetnum, presence: true
 	validates :street, presence: true
